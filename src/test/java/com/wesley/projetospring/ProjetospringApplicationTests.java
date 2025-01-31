07b7ab94e363f57774bbcda0384dd042913dbd4f
wesleypds <wlypereiraa@gmail.com>
2025-01-31 15:48:17 -0300

    inicializando projeto

---
+++ src/test/java/com/wesley/projetospring/ProjetospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.projetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

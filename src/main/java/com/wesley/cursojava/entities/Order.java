5d9955b9bf78b0084312696a663c109921dbe9dc
wesleypds <wlypereiraa@gmail.com>
2025-02-02 18:24:45 -0300

    :construction: wip: add enum OrderStatus e configurando

--- src/main/java/com/wesley/cursojava/entities/Order.java
+++ src/main/java/com/wesley/cursojava/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wesley.cursojava.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,6 +24,8 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss:'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -30,9 +33,10 @@ public class Order implements Serializable {
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,14 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
@@ -84,4 +96,5 @@ public class Order implements Serializable {
             return false;
         return true;
     }
+
 }

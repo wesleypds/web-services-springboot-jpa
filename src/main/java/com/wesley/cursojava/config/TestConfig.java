97af55f49c09f9254036ce3564ddf124bc86bdcb
wesleypds <wlypereiraa@gmail.com>
2025-02-04 11:16:51 -0300

    :sparkles: feat: add comentários no código

--- src/main/java/com/wesley/cursojava/config/TestConfig.java
+++ src/main/java/com/wesley/cursojava/config/TestConfig.java
@@ -43,23 +43,27 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        // Instanciando usuários e salvando no banco
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
+        // Instanciando pedidos e salvando no banco
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+        // Instanciando categorias e salvando no banco
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
+        // Instanciando produtos e salvando no banco
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         p1.getCategories().add(cat1);
 
@@ -77,6 +81,7 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        // Instanciando itens de pedido e salvando no banco
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
@@ -84,6 +89,7 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        // Adicionando pagamentos à pedidos e salvando no banco
         Payment pa1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
         o1.setPayment(pa1);
 

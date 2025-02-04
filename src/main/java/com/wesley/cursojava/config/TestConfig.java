1253e5ce6a43a640f411cbb9f2630c777f41e26b
wesleypds <wlypereiraa@gmail.com>
2025-02-04 11:12:20 -0300

    add associação entre pedido e pagamento

--- src/main/java/com/wesley/cursojava/config/TestConfig.java
+++ src/main/java/com/wesley/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wesley.cursojava.entities.Category;
 import com.wesley.cursojava.entities.Order;
 import com.wesley.cursojava.entities.OrderItem;
+import com.wesley.cursojava.entities.Payment;
 import com.wesley.cursojava.entities.Product;
 import com.wesley.cursojava.entities.User;
 import com.wesley.cursojava.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pa1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+        o1.setPayment(pa1);
+
+        orderRepository.save(o1);
 
     }
 

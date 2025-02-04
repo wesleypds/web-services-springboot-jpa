c39154a89edb5449ba32991e06c0a7bd2736a2c9
wesleypds <wlypereiraa@gmail.com>
2025-02-04 10:55:49 -0300

    :construction: wip: add associação entre pedidos e produtos

--- src/main/java/com/wesley/cursojava/entities/Product.java
+++ src/main/java/com/wesley/cursojava/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +37,9 @@ public class Product implements Serializable {
                 joinColumns = @JoinColumn(name = "product_id"), 
                 inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -88,6 +94,13 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> orders = new HashSet<>();
+        items.forEach(x -> orders.add(x.getOrder()));
+        return orders;
     }
 
     @Override
